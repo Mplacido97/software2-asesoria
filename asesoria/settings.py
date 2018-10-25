8902a4de4d80985104288759e30245d65ff056ae
Miguel Placido <39931127+Mplacido97@users.noreply.github.com>
2018-10-24 22:31:36 -0500

    Update settings.py

--- asesoria/settings.py
+++ asesoria/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = "/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # LOGIN_REDIRECT_URL = '/account/home'
 
 STATICFILES_DIRS = (
